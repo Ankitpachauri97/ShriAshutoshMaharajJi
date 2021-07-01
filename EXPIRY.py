b1166070ff08222a1be1f33fe2ac62060b1636a3
Ankitpachauri97 <ankitpachauri1234@gmail.com>
2021-07-01 09:27:25 +0530

    change

--- EXPIRY.py
+++ EXPIRY.py
@@ -24,16 +24,19 @@ kite.set_access_token(Credentials.access_token)
 # hard coded the number of positions
 position=kite.positions()
 all_positions=position["net"]
+pprint(all_positions)
 
-		#*****************DEFINING variables****************************************************************************************************************
+		#DEFINING variables---------------------------------------------------------------------------------------------------------------------------------
+
 Position_tokens=[]
 Positions_symbol=[]
 Positions_instruementtockens=[]
 Quantity=[]
-Date="6/17/2021"
+Date="7/1/2021"
 
 for n in all_positions:
 	if n["product"]== "MIS" and n["sell_price"]>0 and n["quantity"]<0:
+
 		# position_tokens[n["instrument_token"]]={"SELL":n["average_price"],"CURRENT":0,"Quantity":abs(n["quantity"]),"TRADINGSYMBOL":n["tradingsymbol"]}
 		token={n["instrument_token"]:{"SELL":round(n["average_price"],1),"CURRENT":round(0,1),"QUANTITY":abs(n["quantity"]),"TRADINGSYMBOL":n["tradingsymbol"]}}
 		Position_tokens.append(token)
@@ -120,6 +123,10 @@ Ticker_instruement_dict=dict(zip(instrument_token,Ticker))
 
 		#Creating BUYING and SELLING functions to Place orders-----------------------------------------------------------------------------------------------
 
+def on_close(ws, code, reason):
+    ws.stop()
+
+
 Quantity=abs(Quantity[0])
 Product="kite.PRODUCT_"+PRODUCT_MIS_NRML
 
@@ -140,6 +147,30 @@ def ApplyOrders(buying_back,selling_lot,Quantity):
                                 order_type=kite.ORDER_TYPE_MARKET,
                                 product=kite.PRODUCT_MIS)
 
+		#important Order Placement in case of Cascade Effect ------------------------------------------------------------------------------------------------
+
+def SquareOffAllPositions():
+		FirstSquareOff=Ticker_instruement_dict[instrument_first_position]
+		SecondSquareOff=Ticker_instruement_dict[instrument_second_position]
+		kite.place_order(tradingsymbol=FirstSquareOff,
+                                exchange=kite.EXCHANGE_NFO,
+                                variety=kite.VARIETY_REGULAR,
+                                transaction_type=kite.TRANSACTION_TYPE_BUY,
+                                quantity=Quantity,
+                                order_type=kite.ORDER_TYPE_MARKET,
+                                product=kite.PRODUCT_MIS)
+
+		kite.place_order(tradingsymbol=SecondSquareOff,
+                                exchange=kite.EXCHANGE_NFO,
+                                variety=kite.VARIETY_REGULAR,
+                                transaction_type=kite.TRANSACTION_TYPE_BUY,
+                                quantity=Quantity,
+                                order_type=kite.ORDER_TYPE_MARKET,
+                                product=kite.PRODUCT_MIS)
+
+		kws.on_close = on_close
+
+
 def get_key(val):
     for key, value in Ticker_Instrument_token_dict.items():
         if val == value:
@@ -149,9 +180,13 @@ def get_key(val):
 
 instrument_token_LTP=dict.fromkeys(instrument_token,"-1")
 
+Red_flag=0
+gameover=0
 def on_ticks(ws, ticks):
 	global instrument_first_position
 	global instrument_second_position
+	global Red_flag
+	global gameover
 
 	for token in ticks:
 		token_value=token["instrument_token"]
@@ -175,12 +210,25 @@ def on_ticks(ws, ticks):
 	unrealised_second_position=(position_token_LTP_LAST[instrument_second_position]["SELL"]-position_token_LTP_LAST[instrument_second_position]["CURRENT"])*Quantity
 
 	print(int(unrealised_first_position),int(unrealised_second_position))
+	print(Red_flag)
 
 			#spliting the instrument_token_LTP into  CE and PE lists---------------------------------------------------------------------------------------
 
 	instrument_token_LTP_CE_check={}
 	instrument_token_LTP_PE_check={}
+	print()
 
+			#Getting the individual and complete profit and losses-----------------------------------------------------------------------------------------
+
+
+	individual_pnl=[]
+	total_pnl=0
+	for n in individual_pnl:
+		total_pnl=total_pnl + int(n)
+
+	if len(individual_pnl)>0:
+		print(individual_pnl)
+		print(total_pnl)
 
 	n=0
 	for key, value in instrument_token_LTP.items():
@@ -191,35 +239,58 @@ def on_ticks(ws, ticks):
 			instrument_token_LTP_PE_check.update({key:value})
 			n+=1
 
- #************************************************* IMP CODE TO PUT BUYING/SELLING ORDERS**********************************************************************************
+ #************************************************* IMP CODE TO PUT BUYING/SELLING ORDERS*******************************************************************
+
 
  			##Moving down the calls-------------------------------------------------------------------------------------------------------------------------
 
 	if unrealised_first_position<unrealised_second_position:
+		print("Approching the Downside")
 		Closest_instrument_value=min(instrument_token_LTP_CE_check, key=lambda y:abs(float(instrument_token_LTP_CE_check[y])-instrument_token_LTP[instrument_first_position]))
 		if Closest_instrument_value != instrument_second_position and Ticker_instruement_dict[Closest_instrument_value][-7:-2] < Ticker_instruement_dict[instrument_second_position][-7:-2] and (instrument_token_LTP[instrument_first_position]-instrument_token_LTP_CE_check[Closest_instrument_value])>min(instrument_token_LTP[instrument_first_position]/4,6):
-			buying_back = get_key(instrument_second_position)
-			selling_lot = get_key(Closest_instrument_value)
-			print(selling_lot,buying_back)
-			ApplyOrders(buying_back,selling_lot,Quantity)
-			del position_token_LTP_LAST[instrument_second_position]
-			instrument_second_position=Closest_instrument_value
-			position_token_LTP_LAST[instrument_second_position] = {"SELL": instrument_token_LTP_CE_check[Closest_instrument_value], "CURRENT": instrument_token_LTP_CE_check[Closest_instrument_value],"TRADINGSYMBOL":Ticker_instruement_dict[Closest_instrument_value]}
+			print(get_key(Closest_instrument_value))
+			print("i'm this close to this value:",(instrument_token_LTP[instrument_first_position]-instrument_token_LTP_CE_check[Closest_instrument_value]))
+
+			if gameover<1:
+				if (Red_flag>(int(END[-7:-2])-int(START[-7:-2]))/50) and (gameover<1):
+					gameover+=1
+					SquareOffAllPositions()				
+				else:
+					Red_flag+=1
+					buying_back = get_key(instrument_second_position)
+					selling_lot = get_key(Closest_instrument_value)
+					print(selling_lot,buying_back)
+					individual_pnl.append(unrealised_second_position)
+					ApplyOrders(buying_back,selling_lot,Quantity)
+					del position_token_LTP_LAST[instrument_second_position]
+					instrument_second_position=Closest_instrument_value
+					position_token_LTP_LAST[instrument_second_position] = {"SELL": instrument_token_LTP_CE_check[Closest_instrument_value], "CURRENT": instrument_token_LTP_CE_check[Closest_instrument_value],"TRADINGSYMBOL":Ticker_instruement_dict[Closest_instrument_value]}
 
 			##Moving up the puts ----------------------------------------------------------------------------------------------------------------------------
 		
 	elif unrealised_first_position>unrealised_second_position:
+		print("Approching the upside")
 		Closest_instrument_value=min(instrument_token_LTP_PE_check, key=lambda y:abs(float(instrument_token_LTP_PE_check[y])-instrument_token_LTP[instrument_second_position]))
 		if Closest_instrument_value!=instrument_first_position and Ticker_instruement_dict[Closest_instrument_value][-7:-2] > Ticker_instruement_dict[instrument_first_position][-7:-2] and  (instrument_token_LTP[instrument_second_position]-instrument_token_LTP_PE_check[Closest_instrument_value])>min(instrument_token_LTP[instrument_second_position]/4,6):
-	 		buying_back = get_key(instrument_first_position)
-	 		selling_lot = get_key(Closest_instrument_value)
-	 		print(selling_lot,buying_back)
-	 		ApplyOrders(buying_back,selling_lot,Quantity)
-	 		del position_token_LTP_FIRST[instrument_first_position]
-	 		instrument_first_position=Closest_instrument_value
-	 		position_token_LTP_FIRST[instrument_first_position] = {"SELL": instrument_token_LTP_PE_check[Closest_instrument_value], "CURRENT": instrument_token_LTP_PE_check[Closest_instrument_value],"TRADINGSYMBOL":Ticker_instruement_dict[Closest_instrument_value]}
-
-
+				
+			print(get_key(Closest_instrument_value))
+			print(("i'm this close to this value:",~instrument_token_LTP[instrument_second_position]-instrument_token_LTP_CE_check[Closest_instrument_value]))
+			if gameover<1:
+				if (Red_flag>abs(int(END[-7:-2])-int(START[-7:-2]))/50) and (gameover<1):
+					gameover+=1
+					SquareOffAllPositions()
+				
+				else:
+					Red_flag+=1
+					buying_back = get_key(instrument_first_position)
+					selling_lot = get_key(Closest_instrument_value)
+					print(selling_lot,buying_back)
+					individual_pnl.append(unrealised_first_position)
+					ApplyOrders(buying_back,selling_lot,Quantity)
+					del position_token_LTP_FIRST[instrument_first_position]
+					instrument_first_position=Closest_instrument_value
+					position_token_LTP_FIRST[instrument_first_position] = {"SELL": instrument_token_LTP_PE_check[Closest_instrument_value], "CURRENT": instrument_token_LTP_PE_check[Closest_instrument_value],"TRADINGSYMBOL":Ticker_instruement_dict[Closest_instrument_value]}
+	 			
 
 # #************************************************* IMP CODE TO PUT BUYING/SELLING ORDERS*************************************************###		
 
@@ -227,7 +298,7 @@ def on_connect(ws, response):
     ws.subscribe(instrument_token)
     ws.set_mode(ws.MODE_FULL,instrument_token )
     
-
 kws.on_ticks = on_ticks
 kws.on_connect = on_connect
+
 kws.connect()
